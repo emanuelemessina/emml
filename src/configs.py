5b6fa39a09eded40479a351b251a3fea5b932ba2
Emanuele Messina <emanuelemessina.em@gmail.com>
2024-10-29 23:34:20 +0100

    created config manager and trainer

---
+++ src/configs.py
@@ -0,0 +1,68 @@
+import hashlib
+import os
+
+
+class ConfigManager:
+    def __init__(self):
+        self.configs = {}
+        self.active = {}
+
+    def add(self, name, model, loss_fn, optim, batch_size, epochs):
+        self.configs[name] = {
+            "model": model,
+            "loss_fn": loss_fn,
+            "optim": optim,
+            "batch_size": batch_size,
+            "epochs": epochs,
+        }
+        return self
+
+    def __repr__(self):
+        return f"{self.configs}"
+
+    def __getitem__(self, key):
+        return self.configs[key]
+
+    def __setitem__(self, key, value):
+        self.configs[key] = value
+
+    def activate(self, name, lab_path):
+        self.active = self.configs[name]
+
+        active = self.active
+
+        model = active["model"]
+        optimizer = active["optim"]
+        batch_size = active["batch_size"]
+        num_epochs = active["epochs"]
+        loss_fn = active["loss_fn"]
+
+        active["checkpoint"] = {
+            "info": {
+                "model_name": model.__class__.__name__,
+                "optimizer_name": optimizer.__class__.__name__,
+                "loss_fn_name": loss_fn.__name__,
+                "batch_size": batch_size,
+                "epochs": num_epochs,
+            },
+            "optim_state": optimizer.state_dict(),
+        }
+
+        checkpoint = active["checkpoint"]
+
+        checkpoint_str = str(checkpoint)
+        hash_object = hashlib.md5(checkpoint_str.encode())
+        hash = hash_object.hexdigest()[:10]
+
+        model_path = os.path.join(lab_path, "model")
+
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+
+        checkpoint_name = f"{checkpoint['info']['model_name']}_{hash}"
+        checkpoint_file = f"{checkpoint_name}.pth"
+        checkpoint_path = os.path.join(model_path, checkpoint_file)
+
+        print(f"Checkpoint: {checkpoint_name}")
+
+        active["checkpoint_path"] = checkpoint_path
---
+++ src/trainer.py
@@ -0,0 +1,154 @@
+import torch
+import numpy as np
+import os
+
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+
+class Trainer:
+    def __init__(self, config):
+
+        self.checkpoint_path = config["checkpoint_path"]
+        self.checkpoint = config["checkpoint"]
+
+        self.model = config["model"]
+        self.loss_fn = config["loss_fn"]
+        self.optimizer = config["optim"]
+        self.num_epochs = config["epochs"]
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        print(f"Using device: {self.device}")
+
+        self.avg_loss_min = np.Inf
+        self.last_epoch = 1
+
+    def restore_checkpoint(self):
+        if os.path.exists(self.checkpoint_path):
+            checkpoint = torch.load(self.checkpoint_path)
+            self.model.load_state_dict(checkpoint["model_state_dict"])
+            self.avg_loss_min = checkpoint["avg_loss"]
+            self.last_epoch = checkpoint["epoch"]
+            # future proof for loading a generic model
+            info = checkpoint["info"]
+
+            print(f"Last epoch: {self.last_epoch}")
+            print(f"Avg loss min: {self.avg_loss_min}")
+
+    def train_epoch(self, train_loader, epoch, plot=False):
+
+        model = self.model
+        optimizer = self.optimizer
+
+        model.train()
+
+        losses = []
+        progresses = []
+
+        plt.ion()
+
+        for batch_idx, (data, target) in enumerate(train_loader):
+
+            data, target = data.to(self.device), target.to(self.device)
+
+            optimizer.zero_grad()
+
+            output = model(data)
+
+            loss = self.loss_fn(output, target)
+
+            loss.backward()
+
+            optimizer.step()
+
+            batch_size = data.shape[0]
+            if batch_idx % batch_size == 0:
+                sample_idx = batch_idx * len(data)
+                num_samples = len(train_loader.dataset)
+                num_batches = len(train_loader)
+                progress = 100.0 * batch_idx / num_batches
+
+                losses.append(loss.item())
+                progresses.append(progress)
+
+                if plot:
+                    clear_output(wait=True)
+                    plt.figure(figsize=(10, 6))
+                    plt.plot(progresses, losses, label="Loss")
+                    plt.xlabel("Progress (%)")
+                    plt.ylabel("Loss")
+                    plt.title(f"Epoch {epoch}")
+                    plt.legend()
+                    plt.grid(True)
+                    plt.show()
+
+                print(
+                    f"Train Epoch: {epoch} [s {sample_idx+1}/{num_samples} b {batch_idx+1}/{num_batches} ({progress:.0f}%)]\tLoss: {loss.item():.6f}"
+                )
+
+    def test(self, loader):
+        """
+        returns the average loss on the provided set
+        """
+
+        model = self.model
+
+        model.eval()
+        avg_loss = 0
+        correct = 0
+
+        for data, target in loader:
+            batch_size = data.shape[0]
+            data, target = data.to(self.device), target.to(self.device)
+            output = model(data)
+            avg_loss += self.loss_fn(output, target, reduction="sum").item()
+
+            pred = output.argmax(dim=1, keepdim=True)
+
+            # sanity check
+            pred = pred.view(batch_size)  # [bs,]
+            target = target.view(batch_size)  # [bs,]
+
+            # compute prediction ok
+            batch_pred_ok = pred.eq(target).sum().item()
+            correct += batch_pred_ok
+
+        avg_loss /= len(loader.dataset)
+        num_samples = len(loader.dataset)
+        accuracy = 100.0 * correct / num_samples
+        print(f"Eval average loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}%")
+        return avg_loss, accuracy
+
+    def val(self, loader, epoch, avg_loss_min):
+        """
+        saves the model if the new avg loss is less than the provided minimum,
+        returns the current minimum average loss
+        """
+
+        avg_loss, accuracy = self.test(loader)
+
+        if avg_loss <= avg_loss_min:
+
+            print(
+                f"Validation loss decreased: {avg_loss_min} ----> {avg_loss} , Accuracy: {accuracy:.3f}% , Saving Model..."
+            )
+
+            torch.save(
+                {
+                    "model_state_dict": self.model.state_dict(),
+                    "avg_loss": avg_loss,
+                    "epoch": epoch,
+                    "info": self.info,
+                },
+                self.checkpoint_path,
+            )
+
+            return avg_loss
+
+        return avg_loss_min
+
+    def train_loop(self, train_loader, val_loader, plot=False):
+        for epoch in range(self.last_epoch, self.num_epochs + 1):
+            self.train(train_loader, epoch, plot)
+            avg_loss_min = self.val(val_loader, epoch, avg_loss_min)
